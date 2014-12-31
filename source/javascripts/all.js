781664e906f26a195e393c0745c5b30fd1130549
Andrew Reifman <andrewr23@gmail.com>
2014-12-31 01:35:35 -0500

    kickdrop case study build

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,4 +6,71 @@ $(document).ready(function(){
     $(this).toggleClass('open');
   });
 
+  function isScrolledIntoView(elem){
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((docViewTop < elemTop) && (docViewBottom > elemBottom));
+  }
+
+  $(window).on('scroll', function (){
+    if(isScrolledIntoView('.themes')) {
+      setTimeout(function(){
+        $('.theme-1').show().delay(200).animate({'margin-left': '-420px'}, 300, 'easeOutBack');
+        $('.theme-2').show().delay(200).animate({'margin-left': '-350px'}, 300, 'easeOutBack');
+        $('.theme-3').show().delay(200).animate({'margin-left': '-150px'}, 300, 'easeOutBack');
+        $('.theme-4').show().delay(200).animate({'margin-left': '-80px'}, 300, 'easeOutBack');
+      }, 2600);
+
+      $('.theme-main').addClass('open');
+      setTimeout(function(){
+        $('.theme-main .image').show();
+      }, 1400);
+    }
+  });
+
+  // Drag and drop widgets
+  $(".widgets .widget").each(function(){
+    $(this).mousedown(function(){
+      var boxClass = $(this).attr('title');
+      $(".box." + boxClass + "").addClass('active animated rubberBand');
+      $(".bubble").addClass('animated bounceOut');
+    });
+
+    $(this).mouseup(function(){
+      var boxClass = $(this).attr('title');
+      $(".box." + boxClass + "").removeClass('active animated rubberBand');
+      $(".bubble").hide();
+    });
+  });
+
+  $(".widgets .widget").draggable({
+    revert: "invalid",
+    revertDuration: 500
+  });
+
+  $(".browser-content .box").each(function(){
+    var widgetClass = $(this).attr('title');
+    $(this).droppable({
+      tolerance: "touch",
+      accept: function(e){
+        if(e.hasClass(widgetClass)) return true;
+      },
+      drop: function( event, ui ) {
+        $(".widget." + widgetClass + "").addClass("animated tada");
+        $(".widget." + widgetClass + "").draggable("disable");
+        $(ui.draggable).detach().css({top: 0,left: 0}).appendTo(this);
+        $(this).addClass('complete');
+
+        if ($('.browser-content .complete').length == 6) {
+          $('.drops').addClass('completed');
+          $('.drops .text.mobile').addClass('bounceOutUp');
+        }
+      }
+    });
+  });
+
 });
